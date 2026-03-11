d881c7cac80c41b1d62b695bc454779c537956db
tsgabrielle deploy <contact@tsgabrielle.us>
2026-03-11 19:04:11 -0400

    chore: downgrade to next 15 to fix build, and comment turbopack in config

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   // turbopack: {
     // root: process.cwd()
-  },
+  // },
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "*.supabase.co" },
